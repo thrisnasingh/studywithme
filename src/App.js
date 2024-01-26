d17856861e4efe75deb3a55fe071419a0f4ab5b2
quekyq <queky@seas.upenn.edu>
2024-01-26 17:16:22 -0500

    Start app.js and app.css. Add assets

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
+import help from './assets/help.svg';
+import music from './assets/music.svg';
+import stickies from './assets/stickies.svg';
+import stress from './assets/stress.svg';
+import landing from './assets/landing.png';
 import './App.css';
 
+
 function App() {
+  // Declare variables
+
+
+  //Write any functions
+  // function sayHello () {
+  //   console.log('hello')
+  // }
+
+  //return HTML
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <div className="nav">
+        <div className='button'>
+          <img src={music} className="btn" alt="music" />
+          <img src={stickies} className="btn" alt="stickies" />
+          <img src={stress} className="btn" alt="stress" />
+          <img src={help} className="btn" alt="help" />
+        </div>
+      </div>
+      
+
+        <div className='session-start'>
+          
+          <img src={landing} className="home-img" alt="help" />
+          <div className='start-btn'>
+            <p>Begin new study session</p>
+            </div>
+
+        </div>
+
+
+        <div className='set-time'>
+        
+        </div>
+    
+  </div>
+      
+    
   );
 }
 
