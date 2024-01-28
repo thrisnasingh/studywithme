c2914a006038c155493b7e60d31bec3a2a018be4
quekyq <queky@seas.upenn.edu>
2024-01-28 09:03:45 -0500

    add duck ending image

--- src/containers/EndSessionContainer.js
+++ src/containers/EndSessionContainer.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import '../containers-css/EndSessionContainer.css';
 import catEnd from '../assets/catend.png'
 import frogEnd from '../assets/frogend.png'
-import duckEnd from '../assets/catend.png'
+import duckEnd from '../assets/duckend.png'
 import HomeContainer from './HomeContainer';
 import backhome from '../assets/backhome.svg'
 
